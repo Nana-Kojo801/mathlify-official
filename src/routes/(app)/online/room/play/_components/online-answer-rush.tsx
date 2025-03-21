7759838549a1e8bd0c8119a2bd4d885bbc497754
Nana Kojo <nkabenyah@gmail.com>
2025-03-20 21:14:13 -0700

    Refactor game state handling in OnlineAnswerRushPlayPage component

--- src/routes/(app)/online/room/play/_components/online-answer-rush.tsx
+++ src/routes/(app)/online/room/play/_components/online-answer-rush.tsx
@@ -7,23 +7,40 @@ import { useEffect } from "react";
 import { useOutletContext } from "react-router";
 import { OutletContext } from "../../_types";
 import AnswerRushResults from "./answer-rush-results";
+import { useQuery } from "@tanstack/react-query";
+import { convexQuery } from "@convex-dev/react-query";
 
 const OnlineAnswerRushPlayPage = () => {
   const user = useLiveUser();
-  const { roomId, room } = useOutletContext<OutletContext>();
+  const { roomId } = useOutletContext<OutletContext>();
   const setState = useAnswerRushGameStore((store) => store.setState);
   const score = useAnswerRushGameStore((store) => store.score);
   const updateScore = useConvexMutation(api.rooms.updateAnswerRushScore);
 
+  // Subscribe to game state
+  const { data: gameState } = useQuery(
+    convexQuery(api.games.getGameState, {
+      roomId,
+    })
+  );
+
   useEffect(() => {
-    updateScore({ score, roomId, userId: user._id, gameId: room.currentGameId });
+    if (!gameState?.currentGameId) return;
+    updateScore({ 
+      score, 
+      roomId, 
+      userId: user._id, 
+      gameId: gameState.currentGameId 
+    });
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [score]);
 
   useEffect(() => {
-    setState("countdown");
+    if (gameState?.phase === "playing") {
+      setState("countdown");
+    }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [gameState?.phase]);
 
   return <AnswerRush CustomResults={AnswerRushResults} />;
 };
